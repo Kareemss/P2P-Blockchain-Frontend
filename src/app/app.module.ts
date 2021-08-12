1e8e49ef6f17e20736851ba4487a6b5efe56526e
Drayang <dchu0011@student.monash.edu>
2021-08-12 21:11:41 +0800

    Add new login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TransactionComponent } from './transaction/transaction.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { MainPageComponent } from './main-page/main-page.component';
     TransactionComponent,
     routingComponents,
     UserProfileComponent,
-    MainPageComponent
+    MainPageComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
