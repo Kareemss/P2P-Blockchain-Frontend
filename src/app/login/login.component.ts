80b9d0ea430b095a375cb7e9a2266aadce7aa123
Mohammad Rafaquat <mala0031@student.monash.edu>
2021-09-04 22:31:18 +0800

    Store encrypted session storage in local storage of browser

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,9 @@ import { User } from '../block';
 import { HttpService } from '../http.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { throwError } from 'rxjs';
+//Inside imports of your TS file include 
+import * as CryptoJS from 'crypto-js';
+
 
 
 @Component({
@@ -16,6 +19,11 @@ export class LoginComponent implements OnInit {
   validatedUser = new User();
   constructor(private _http: HttpClientModule, private _httpService: HttpService, private route:ActivatedRoute , private router: Router) { }
 
+  // Declare this key and iv values in declaration
+  private key = CryptoJS.enc.Utf8.parse('4512631236589784');
+  //console.log(key)
+  private iv = CryptoJS.enc.Utf8.parse('4512631236589784');
+
   ngOnInit(): void {
   }
   // compare to the user profile stoered in DB
@@ -26,18 +34,56 @@ export class LoginComponent implements OnInit {
     this._httpService.loginUser(this.validatedUser)
     .subscribe(data=> {
       console.log(data)
-  
-      if (data == true){
+      //this.encryptUsingAES256();
+      if (data.Res == true){
         this.router.navigate(['../landingPage'],{relativeTo: this.route});
-       
+        this.createSessionToken(data.Email, data.PasswordHash)
       }
       else {
         document.getElementById("error")!.innerHTML = "<p><h2>Incorrect Email or Password!!</h2></p>";
       }
     })
+  }
+
+  createSessionToken(email: string, passwordHash: string){
+    console.log(email)
+    console.log(passwordHash)
+    let token = {
+      userEmail : this.encryptUsingAES256(email),
+      userPassword : this.encryptUsingAES256(passwordHash)
+    }
+    // store cookie in local storage
+    localStorage.setItem("session-token", JSON.stringify(token))
+
+    //JSON.parse(localStorage.getItem("session-token"));
     
   }
+
+  encryptUsingAES256(plainText: string) {
+    var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(JSON.stringify(plainText)), this.key, {
+        keySize: 128 / 8,
+        iv: this.iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    console.log('Encrypted :\n' + encrypted.toString());
+    //console.log(this.key);
+    return encrypted.toString();
+  }
+
+    
+  decryptUsingAES256(decString: string) {
+    var decrypted = CryptoJS.AES.decrypt(decString, this.key, {
+        keySize: 128 / 8,
+        iv: this.iv,
+        mode: CryptoJS.mode.CBC,
+        padding: CryptoJS.pad.Pkcs7
+    });
+    //console.log('Decrypted : ' + decrypted);
+    console.log('Decrypted = ' + decrypted.toString(CryptoJS.enc.Utf8));
+  }
   
+
   goBack(){
     this._httpService.goBack();
   }
