be344cd330745c38d433051da6797a9b90873011
Mohammad Rafaquat <mala0031@student.monash.edu>
2021-09-05 13:59:21 +0800

    Retrieve session token and decrypt the email and password

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,7 @@ import * as CryptoJS from 'crypto-js';
 
 
 
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -34,16 +35,50 @@ export class LoginComponent implements OnInit {
     this._httpService.loginUser(this.validatedUser)
     .subscribe(data=> {
       console.log(data)
-      //this.encryptUsingAES256();
       if (data.Res == true){
         this.router.navigate(['../landingPage'],{relativeTo: this.route});
         this.createSessionToken(data.Email, data.PasswordHash)
+
+        // get encrypted session token as a string 
+        let tknStr = localStorage.getItem("session-token")
+        console.log("Retrieved session token")
+        console.log(tknStr)
+        // split the string in two parts, userEmail and userPassword
+        let tknArr = tknStr?.split(",")
+        //console.log(tknArr)
+
+        // retrieve session token if not undefined
+        if (tknArr != undefined){
+          this.retrieveSessionToken(tknArr)
+        }
       }
       else {
         document.getElementById("error")!.innerHTML = "<p><h2>Incorrect Email or Password!!</h2></p>";
       }
     })
   }
+
+  retrieveSessionToken(tknArr : string[]){
+    // split the token array into two separate parts
+    let uEmailArr = tknArr[0].split('\"')
+    let uPassArr = tknArr[1].split('\"')
+
+    // encrypted value at index 3 for both email and password
+    let encEmail = uEmailArr[3]
+    let encPass = uPassArr[3]
+    
+    // decrypt the email and password
+    let decEmail = this.decryptUsingAES256(encEmail)
+    let decPass = this.decryptUsingAES256(encPass)
+
+    
+    console.log("Encrypted email: ", encEmail)
+    console.log("Decrypted email: ", decEmail)
+    console.log("Encrypted password: ", encPass)
+    console.log("Decrypted password hash: ", decPass)
+
+  }
+
 
   createSessionToken(email: string, passwordHash: string){
     console.log(email)
@@ -54,10 +89,8 @@ export class LoginComponent implements OnInit {
     }
     // store cookie in local storage
     localStorage.setItem("session-token", JSON.stringify(token))
-
-    //JSON.parse(localStorage.getItem("session-token"));
-    
   }
+
 
   encryptUsingAES256(plainText: string) {
     var encrypted = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(JSON.stringify(plainText)), this.key, {
@@ -66,7 +99,7 @@ export class LoginComponent implements OnInit {
         mode: CryptoJS.mode.CBC,
         padding: CryptoJS.pad.Pkcs7
     });
-    console.log('Encrypted :\n' + encrypted.toString());
+    //console.log('Encrypted :\n' + encrypted.toString());
     //console.log(this.key);
     return encrypted.toString();
   }
@@ -80,7 +113,8 @@ export class LoginComponent implements OnInit {
         padding: CryptoJS.pad.Pkcs7
     });
     //console.log('Decrypted : ' + decrypted);
-    console.log('Decrypted = ' + decrypted.toString(CryptoJS.enc.Utf8));
+    //console.log('Decrypted = ' + decrypted.toString(CryptoJS.enc.Utf8));
+    return decrypted.toString(CryptoJS.enc.Utf8);
   }
   
 
